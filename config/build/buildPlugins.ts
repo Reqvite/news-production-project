a80bfeea27b800c0ab4fe60a9e1ca191b71e647d
Reqvite <reqvite2233@gmail.com>
2023-05-08 18:10:57 +0300

    add HotModuleReplacementPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,5 +1,10 @@
 import HTMLWebpackPlugin from "html-webpack-plugin";
-import { DefinePlugin, ProgressPlugin, WebpackPluginInstance } from "webpack";
+import {
+  DefinePlugin,
+  HotModuleReplacementPlugin,
+  ProgressPlugin,
+  WebpackPluginInstance,
+} from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BuildOptions } from "./types/config";
 
@@ -19,5 +24,6 @@ export function buildPlugins({
     new DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
+    new HotModuleReplacementPlugin(),
   ];
 }
