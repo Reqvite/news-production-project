65fe80f010638ac8280e7f473ac8d32668748433
Reqvite <reqvite2233@gmail.com>
2023-05-16 20:42:53 +0300

    upd

--- src/widgets/SideBar/ui/SideBar/Sidebar.stories.tsx
+++ src/widgets/SideBar/ui/SideBar/Sidebar.stories.tsx
@@ -1,8 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { withRouter } from 'storybook-addon-react-router-v6';
+
 import { SideBar } from './SideBar';
 
 const meta: Meta<typeof SideBar> = {
     title: 'widget/SideBar',
+    decorators: [withRouter],
     component: SideBar,
 };
 
