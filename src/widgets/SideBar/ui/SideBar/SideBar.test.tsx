aa739078e4ba941aafec96ed65c5d2a8100c62c9
Reqvite <reqvite2233@gmail.com>
2023-05-17 11:59:49 +0300

    fix test

--- src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
@@ -1,11 +1,11 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithTranslation } from
-    'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/config/tests/componentRender/ComponentRender';
+
 import { SideBar } from 'widgets/SideBar/';
 
 describe('Sidebar', () => {
     test('test sidebar toggle', () => {
-        renderWithTranslation(<SideBar />);
+        componentRender(<SideBar />);
         const toogleBtn = screen.getByTestId('sidebar-toggle');
         const sidebar = screen.getByTestId('sidebar');
         expect(sidebar).toBeInTheDocument();
