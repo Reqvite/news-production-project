e8dd7bacdd20f2160364d2ca3e822c2bcdb7a6b4
Reqvite <reqvite2233@gmail.com>
2023-05-21 17:07:06 +0300

    fix

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import {
     MouseEvent, ReactNode, useCallback, useEffect, useRef, useState,
 } from 'react';
+import { useTheme } from 'app/providers/ThemeProvider';
 import cls from './Modal.module.scss';
 import { Portal } from '../Portal/Portal';
 
@@ -19,6 +20,8 @@ export const Modal = (props: ModalProps) => {
     const {
         className, children, isOpen, onClose,
     } = props;
+
+    const { theme } = useTheme();
 
     const [isClosing, setiIsClosing] = useState(false);
     const timeRef = useRef <ReturnType<typeof setTimeout>>();
@@ -61,7 +64,7 @@ export const Modal = (props: ModalProps) => {
 
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className])}>
+            <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
                 <div className={cls.overlay} onClick={closeHandler}>
                     <div className={cls.content} onClick={onContentClick}>
                         {children}
